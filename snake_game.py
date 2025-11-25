c968ed5f6e4864ee3ee90f870db31fc182de0002
Genarcarte <bang.bang.rabang@gmail.com>
2025-11-25 18:55:36 +0800

    feat: add screen setup in snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -2,3 +2,9 @@ import pygame
 
 pygame.init()
 
+# Screen setup
+WIDTH = 600     # Width of the screen in pixels
+HEIGHT = 400    # Height of the screen in pixels
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # Create the screen
+pygame.display.set_caption("Snake Game")  # Set the title of the window
